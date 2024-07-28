887b98efd4da55efeac73d6b03a732264ff93e49
Jamie <Jamie.witcher@yahoo.co.uk>
2024-07-28 15:39:59 +0100

    read video and save video code worked

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from utils import read_video, save_video
 
 def main():
-    print("Hello World!")
+    # Read Video
+    video_frames = read_video(r"C:\Users\44780\Documents\new_motion_tracking\input_videos\counter_attack.mp4")
+
+    # Save Video
+    save_video(video_frames, r"C:\Users\44780\Documents\new_motion_tracking\output_video\output_video.avi")
 
 if __name__ == "__main__":
     main()
