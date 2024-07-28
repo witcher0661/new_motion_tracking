46920cbb9e3e915de148009df1d2412784e73a29
Jamie <Jamie.witcher@yahoo.co.uk>
2024-07-28 21:44:32 +0100

    tracker code added for supervision

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -3,9 +3,9 @@ import supervision as sv
 
 class Tracker:
     def __init__(self, model_path):
-        self.model = YOLO(model_path)
+        self.model = YOLO(model_path) 
         self.tracker = sv.ByteTrack()
-
+  
     def detect_frames(self, frames):
         batch_size=20 
         detections = [] 
@@ -14,3 +14,16 @@ class Tracker:
             detections += detections_batch
             break
         return detections
+
+    def get_object_tracks(self, frames):
+
+        detections = self.detect_frames(frames)
+
+        for frame_num, detection in enumerate(detections):
+            cls_names = detection.names
+            cls_names_inv = {v:k for k,v in cls_names.items()}
+
+            # Covert to supervision Detection format
+            detection_supervision = sv.Detections.from_ultralytics(detection)
+
+            print(detection_supervision)
