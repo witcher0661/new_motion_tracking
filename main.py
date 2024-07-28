56e23a23b7ea8bf9a8a80cce7b4b47c03d3433ad
Jamie <Jamie.witcher@yahoo.co.uk>
2024-07-28 20:02:30 +0100

    tracker added to main

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from utils import read_video, save_video
+from trackers import Tracker
 
 def main():
     # Read Video
     video_frames = read_video(r"C:\Users\44780\Documents\new_motion_tracking\input_videos\counter_attack.mp4")
+
+    # Initialize Tracker
+    tracker = Tracker(r"C:\Users\44780\Documents\new_motion_tracking\models\best.pt")
+
+    tracks = tracker.get_object_tracks(video_frames)
 
     # Save Video
     save_video(video_frames, r"C:\Users\44780\Documents\new_motion_tracking\output_video\output_video.avi")
