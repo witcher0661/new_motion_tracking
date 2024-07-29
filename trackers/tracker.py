92afcbab948f5cabeca3af7991be1d16bc599fc3
Jamie <Jamie.witcher@yahoo.co.uk>
2024-07-29 19:42:15 +0100

    tracker id code input

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -32,7 +32,7 @@ class Tracker:
                 if cls_names[class_id] == "goalkeeper":
                     detection_supervision.class_id[object_ind] = cls_names_inv["player"]
 
+            #Track Objects
+            detection_with_tracks = self.tracker.update_with_detections(detection_supervision)
 
             print(detection_supervision)
-
-            break
