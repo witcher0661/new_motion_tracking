99e91d0804e67b73ecae7423affe2f4db3ffcdba
Jamie <Jamie.witcher@yahoo.co.uk>
2024-07-30 21:55:48 +0100

    dictionary complete

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -20,7 +20,9 @@ class Tracker:
         detections = self.detect_frames(frames)
 
         tracks={
-            
+            "players":[],
+            "referees":[],
+            "ball":[]
         }
 
         for frame_num, detection in enumerate(detections):
