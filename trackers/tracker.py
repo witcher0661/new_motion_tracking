e59d6c27dc3042e3a332723b52246dcc62a45175
Jamie <Jamie.witcher@yahoo.co.uk>
2024-07-31 20:12:49 +0100

    stub path and os imported

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import supervision as sv
 import pickle
+import os
 
 class Tracker:
     def __init__(self, model_path):
@@ -17,6 +18,11 @@ class Tracker:
         return detections
 
     def get_object_tracks(self, frames, read_from_stub=False, stub_path=None):
+
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            with open(stub_path,'rb') as f:
+                tracks = pickle.load(f)
+            return tracks
 
         detections = self.detect_frames(frames)
 
