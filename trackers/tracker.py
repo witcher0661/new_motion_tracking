fa48085404cc500d24a1cfb90d67b6e43b7ffc32
Jamie <Jamie.witcher@yahoo.co.uk>
2024-07-31 20:06:23 +0100

    stub path created and pickle imported

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 import supervision as sv
+import pickle
 
 class Tracker:
     def __init__(self, model_path):
@@ -15,7 +16,7 @@ class Tracker:
             break
         return detections
 
-    def get_object_tracks(self, frames):
+    def get_object_tracks(self, frames, read_from_stub=False, stub_path=None):
 
         detections = self.detect_frames(frames)
 
@@ -62,5 +63,9 @@ class Tracker:
 
                 if cls_id == cls_names_inv['ball']:
                     tracks["ball"][frame_num][1] = {"bbox":bbox}
+
+        if stub_path is not None:
+            with open(stub_path,'wb') as f:
+                pickle.dump(tracks,f) 
 
         return tracks
