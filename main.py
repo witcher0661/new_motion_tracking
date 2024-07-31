bc9f4c501475ee096da33089ffe29cdb69e2dce5
Jamie <Jamie.witcher@yahoo.co.uk>
2024-07-31 20:16:03 +0100

    stubs and pickle added to main

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def main():
     #Initialize Tracker
     tracker = Tracker(r"C:\Users\44780\Documents\new_motion_tracking\models\best.pt")
 
-    tracks = tracker.get_object_tracks(video_frames)
+    tracks = tracker.get_object_tracks(video_frames,
+                                       read_from_stub=True,
+                                       stub_path='stubs/track_stubs.pkl')
 
     # Save Video
     save_video(video_frames, r"C:\Users\44780\Documents\new_motion_tracking\output_video\output_video.avi")
