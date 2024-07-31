d1523979261a6fdc4e458ab6252e9085d9876cb9
Jamie <Jamie.witcher@yahoo.co.uk>
2024-07-31 19:54:03 +0100

    track objects code input

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -41,4 +41,26 @@ class Tracker:
             #Track Objects
             detection_with_tracks = self.tracker.update_with_detections(detection_supervision)
 
+            tracks["players"].append({})
+            tracks["referees"].append({})
+            tracks["ball"].append({})
+
+            for frame_detection in detection_with_tracks:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+                track_id = frame_detection[4]
+
+                if cls_id == cls_names_inv['player']:
+                    tracks["players"][frame_num][track_id] = {"bbox":bbox}
+                
+                if cls_id == cls_names_inv['referee']:
+                    tracks["referees"][frame_num][track_id] = {"bbox":bbox}
+
+            for frame_detection in detection_supervision:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+
+                if cls_id == cls_names_inv['ball']:
+                    tracks["ball"][frame_num][1] = {"bbox":bbox}
+
             print(detection_with_tracks)
